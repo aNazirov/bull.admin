8e2cec8e1f9e1230d76e752e63d3213bf316c50c
aNazirov_ <nazirov.bobur01@mail.ru>
2022-11-01 21:45:12 +0500

    fix: ----

--- src/core/components/shared/Photo/index.tsx
+++ src/core/components/shared/Photo/index.tsx
@@ -59,9 +59,10 @@ export const Photo: React.FC<Props> = ({
         {preview && (
           <a
             href={preview}
+            target="_blank"
             className=" bg-white py-2 px-3 border border-gray-300 rounded-sm shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
           >
-            Загрузить
+            Открыть
           </a>
         )}
         <button
